19d92b93ab73457ef88375b2ad5f0383bffd6a53
Ryan Murphy <rdmurphy@users.noreply.github.com>
2019-01-15 22:34:30 -0600

    Add JS support, full async is a go

--- index.js
+++ index.js
@@ -1,47 +1,63 @@
 // internal
 const fs = require('fs');
 const path = require('path');
+const { promisify } = require('util');
+const readFile = promisify(fs.readFile);
 
 // packages
 const dset = require('dset');
 const dsv = require('d3-dsv');
-const glob = require('tiny-glob/sync');
+const glob = require('tiny-glob');
 const parseJson = require('parse-json');
 const yaml = require('js-yaml');
 
-module.exports = function quaff(rawPath) {
+module.exports = async function quaff(rawPath) {
   const cwd = path.normalize(rawPath);
-  const files = glob('**/*.{json,yaml,yml,csv,tsv}', { absolute: true, cwd });
+  const files = await glob('**/*.{js,json,yaml,yml,csv,tsv}', {
+    absolute: true,
+    cwd,
+  });
 
   const payload = {};
 
-  files.forEach(function(file) {
-    const { name, dir, ext } = path.parse(file);
-    const fileContents = fs.readFileSync(file, 'utf8');
+  await Promise.all(
+    files.map(async function(file) {
+      const { name, dir, ext } = path.parse(file);
 
-    let data;
+      let data;
 
-    if (ext === '.json') {
-      data = parseJson(fileContents, file);
-    } else if (ext === '.yaml' || ext === '.yml') {
-      data = yaml.safeLoad(fileContents);
-    } else if (ext === '.csv') {
-      data = dsv.csvParse(fileContents);
-    } else {
-      data = dsv.tsvParse(fileContents);
-    }
+      if (ext === '.js') {
+        data = require(file);
 
-    // remove the leading path, split into a list, and filter out empty strings
-    const dirs = path
-      .relative(cwd, dir)
-      .split(path.sep)
-      .filter(Boolean);
+        if (typeof data === 'function') {
+          data = await data();
+        }
+      } else {
+        const fileContents = await readFile(file, 'utf8');
 
-    // add the filename to the path part list
-    dirs.push(name);
+        if (ext === '.json') {
+          data = parseJson(fileContents, file);
+        } else if (ext === '.yaml' || ext === '.yml') {
+          data = yaml.safeLoad(fileContents);
+        } else if (ext === '.csv') {
+          data = dsv.csvParse(fileContents);
+        } else {
+          data = dsv.tsvParse(fileContents);
+        }
+      }
 
-    dset(payload, dirs, data);
-  });
+      // remove the leading path, split into a list, and filter out empty strings
+      const dirs = path
+        .relative(cwd, dir)
+        .split(path.sep)
+        .filter(Boolean);
+
+      // add the filename to the path part list
+      dirs.push(name);
+
+      dset(payload, dirs, data);
+    })
+  );
 
   return payload;
 };
--- test/test.js
+++ test/test.js
@@ -1,125 +1,120 @@
 // native
 const assert = require('assert');
 const fs = require('fs');
+const { promisify } = require('util');
+const readFile = promisify(fs.readFile);
 
 // packages
+const assertRejects = require('assert-rejects');
 const dsv = require('d3-dsv');
 const yaml = require('js-yaml');
 
 // internal
 const quaff = require('..');
 
-it('should normalize a trailing extra slash', function() {
-  assert.deepEqual(quaff('./test/source/basic_json/'), {
-    corgis: JSON.parse(
-      fs.readFileSync('./test/source/basic_json/corgis.json', 'utf8')
-    ),
+const readJson = async filepath => JSON.parse(await readFile(filepath, 'utf8'));
+const readYaml = async filepath =>
+  yaml.safeLoad(await readFile(filepath, 'utf8'));
+const readCsv = async filepath =>
+  dsv.csvParse(await readFile(filepath, 'utf8'));
+const readTsv = async filepath =>
+  dsv.tsvParse(await readFile(filepath, 'utf8'));
+
+it('should normalize a trailing extra slash', async () => {
+  assert.deepStrictEqual(await quaff('./test/source/basic_json/'), {
+    corgis: await readJson('./test/source/basic_json/corgis.json'),
   });
 });
 
-it('should return object generated from json', function() {
-  assert.deepEqual(quaff('./test/source/basic_json'), {
-    corgis: JSON.parse(
-      fs.readFileSync('./test/source/basic_json/corgis.json', 'utf8')
-    ),
+it('should return object generated from json', async () => {
+  assert.deepStrictEqual(await quaff('./test/source/basic_json'), {
+    corgis: await readJson('./test/source/basic_json/corgis.json'),
   });
 });
 
-it('should return object generated from yaml', function() {
-  assert.deepEqual(quaff('./test/source/basic_yaml'), {
-    corgis: yaml.safeLoad(
-      fs.readFileSync('./test/source/basic_yaml/corgis.yaml', 'utf8')
-    ),
+it('should return object generated from yaml', async () => {
+  assert.deepStrictEqual(await quaff('./test/source/basic_yaml'), {
+    corgis: await readYaml('./test/source/basic_yaml/corgis.yaml'),
   });
 });
 
-it('should return object generated from yml', function() {
-  assert.deepEqual(quaff('./test/source/basic_yml'), {
-    corgis: yaml.safeLoad(
-      fs.readFileSync('./test/source/basic_yml/corgis.yml', 'utf8')
-    ),
+it('should return object generated from yml', async () => {
+  assert.deepStrictEqual(await quaff('./test/source/basic_yml'), {
+    corgis: await readYaml('./test/source/basic_yml/corgis.yml'),
   });
 });
 
-it('should return object generated from csv', function() {
-  assert.deepEqual(quaff('./test/source/basic_csv'), {
-    corgis: dsv.csvParse(
-      fs.readFileSync('./test/source/basic_csv/corgis.csv', 'utf8')
-    ),
+it('should return object generated from csv', async () => {
+  assert.deepStrictEqual(await quaff('./test/source/basic_csv'), {
+    corgis: await readCsv('./test/source/basic_csv/corgis.csv'),
   });
 });
 
-it('should return object generated from tsv', function() {
-  assert.deepEqual(quaff('./test/source/basic_tsv'), {
-    corgis: dsv.tsvParse(
-      fs.readFileSync('./test/source/basic_tsv/corgis.tsv', 'utf8')
-    ),
+it('should return object generated from tsv', async () => {
+  assert.deepStrictEqual(await quaff('./test/source/basic_tsv'), {
+    corgis: await readTsv('./test/source/basic_tsv/corgis.tsv'),
   });
 });
 
-it('should ignore files that do not match filters', function() {
-  assert.deepEqual(quaff('./test/source/non_match_file'), {
-    corgis: yaml.safeLoad(
-      fs.readFileSync('./test/source/non_match_file/corgis.yaml', 'utf8')
-    ),
+it('should ignore files that do not match filters', async () => {
+  assert.deepStrictEqual(await quaff('./test/source/non_match_file'), {
+    corgis: await readYaml('./test/source/non_match_file/corgis.yaml'),
   });
 });
 
-it('should return object representing data one subdirectory deep', function() {
-  assert.deepEqual(quaff('./test/source/single_depth'), {
-    corgis: JSON.parse(
-      fs.readFileSync('./test/source/single_depth/corgis.json', 'utf8')
-    ),
+it('should return object representing data one subdirectory deep', async () => {
+  assert.deepStrictEqual(await quaff('./test/source/single_depth'), {
+    corgis: await readJson('./test/source/single_depth/corgis.json', 'utf8'),
     others: {
-      malamutes: JSON.parse(
-        fs.readFileSync(
-          './test/source/single_depth/others/malamutes.json',
-          'utf8'
-        )
+      malamutes: await readJson(
+        './test/source/single_depth/others/malamutes.json',
+        'utf8'
       ),
-      corgis: dsv.csvParse(
-        fs.readFileSync('./test/source/basic_csv/corgis.csv', 'utf8')
-      ),
+      corgis: await readCsv('./test/source/basic_csv/corgis.csv', 'utf8'),
     },
   });
 });
 
-it('should return object representing data two subdirectories deep', function() {
-  assert.deepEqual(quaff('./test/source/double_depth'), {
-    corgis: JSON.parse(
-      fs.readFileSync('./test/source/double_depth/corgis.json', 'utf8')
-    ),
+it('should return object representing data two subdirectories deep', async () => {
+  assert.deepStrictEqual(await quaff('./test/source/double_depth'), {
+    corgis: await readJson('./test/source/double_depth/corgis.json', 'utf8'),
     others: {
-      malamutes: JSON.parse(
-        fs.readFileSync(
-          './test/source/double_depth/others/malamutes.json',
-          'utf8'
-        )
+      malamutes: await readJson(
+        './test/source/double_depth/others/malamutes.json',
+        'utf8'
       ),
       outcasts: {
-        cats: dsv.csvParse(
-          fs.readFileSync(
-            './test/source/double_depth/others/outcasts/cats.csv',
-            'utf8'
-          )
+        cats: await readCsv(
+          './test/source/double_depth/others/outcasts/cats.csv',
+          'utf8'
         ),
       },
     },
   });
 });
 
-it('should throw an error when attempting to load empty JSON', function() {
-  const tester = function() {
-    quaff('./test/source/basic_json_empty');
-  };
-
-  assert.throws(tester, /JSONError/);
+it('should throw an error when attempting to load empty JSON', () => {
+  assertRejects(quaff('./test/source/basic_json_empty'), /JSONError/);
 });
 
-it('should throw an error when attempting to load bad JSON', function() {
-  const tester = function() {
-    quaff('./test/source/basic_json_error');
-  };
+it('should throw an error when attempting to load bad JSON', () => {
+  assertRejects(quaff('./test/source/basic_json_error'), /JSONError/);
+});
 
-  assert.throws(tester, /JSONError/);
+it('should return what is exported from a JavaScript file (no function)', async () => {
+  assert.deepStrictEqual(await quaff('./test/source/basic_js'), {
+    corgis: require('./source/basic_js/corgis.js'),
+  });
+});
+
+it('should return what is exported from a JavaScript file (sync function)', async () => {
+  assert.deepStrictEqual(await quaff('./test/source/sync_js'), {
+    corgis: require('./source/sync_js/corgis.js')(),
+  });
+});
+
+it('should return what is exported from a JavaScript file (async function)', async () => {
+  assert.deepStrictEqual(await quaff('./test/source/async_js'), {
+    corgis: await require('./source/async_js/corgis.js')(),
+  });
 });
