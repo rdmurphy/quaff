a29b0c1de1502d59a7a91fae12893ebb3d89a657
Ryan Murphy <rdmurphy@users.noreply.github.com>
2020-07-12 22:06:20 -0700

    Maybe use totalist

--- index.js
+++ index.js
@@ -6,61 +6,56 @@ const path = require('path');
 const archieml = require('archieml');
 const dset = require('dset');
 const dsv = require('d3-dsv');
-const glob = require('tiny-glob');
 const parseJson = require('parse-json');
+const totalist = require('totalist');
 const yaml = require('js-yaml');
 
 module.exports = async function quaff(rawPath) {
 	const cwd = path.normalize(rawPath);
 
-	const files = await glob('**/*.{js,json,yaml,yml,csv,tsv,aml}', {
-		absolute: true,
-		cwd,
-	});
-
 	const payload = {};
 
-	await Promise.all(
-		files.map(async function (file) {
-			const { name, dir, ext } = path.parse(file);
+	await totalist(cwd, async (_, file) => {
+		const { name, dir, ext } = path.parse(file);
 
-			let data;
+		let data;
 
-			if (ext === '.js') {
-				// js path
-				data = require(file);
+		if (ext === '.js') {
+			// js path
+			data = require(file);
 
-				if (typeof data === 'function') {
-					data = await data();
-				}
-			} else {
-				const fileContents = await fs.readFile(file, 'utf8');
-
-				switch (ext) {
-					// json path
-					case '.json':
-						data = parseJson(fileContents, file);
-						break;
-					// yaml paths
-					case '.yaml':
-					case '.yml':
-						data = yaml.safeLoad(fileContents);
-						break;
-					// csv path
-					case '.csv':
-						data = dsv.csvParse(fileContents);
-						break;
-					// tsv path
-					case '.tsv':
-						data = dsv.tsvParse(fileContents);
-						break;
-					// aml path
-					case '.aml':
-						data = archieml.load(fileContents);
-						break;
-				}
+			if (typeof data === 'function') {
+				data = await data();
 			}
+		} else {
+			const fileContents = await fs.readFile(file, 'utf8');
 
+			switch (ext) {
+				// json path
+				case '.json':
+					data = parseJson(fileContents, file);
+					break;
+				// yaml paths
+				case '.yaml':
+				case '.yml':
+					data = yaml.safeLoad(fileContents);
+					break;
+				// csv path
+				case '.csv':
+					data = dsv.csvParse(fileContents);
+					break;
+				// tsv path
+				case '.tsv':
+					data = dsv.tsvParse(fileContents);
+					break;
+				// aml path
+				case '.aml':
+					data = archieml.load(fileContents);
+					break;
+			}
+		}
+
+		if (data) {
 			// remove the leading path, split into a list, and filter out empty strings
 			const dirs = path.relative(cwd, dir).split(path.sep).filter(Boolean);
 
@@ -68,8 +63,8 @@ module.exports = async function quaff(rawPath) {
 			dirs.push(name);
 
 			dset(payload, dirs, data);
-		}),
-	);
+		}
+	});
 
 	return payload;
 };
