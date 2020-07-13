869e0049f2c7a51829609eb369fa639b49eebd08
Ryan Murphy <rdmurphy@users.noreply.github.com>
2020-07-12 22:55:45 -0700

    Tweak to use the provided relative path

--- index.js
+++ index.js
@@ -15,25 +15,25 @@ module.exports = async function quaff(rawPath) {
 
 	const payload = {};
 
-	await totalist(cwd, async (_, file) => {
-		const { name, dir, ext } = path.parse(file);
+	await totalist(cwd, async (rel, abs) => {
+		const { name, dir, ext } = path.parse(rel);
 
 		let data;
 
 		if (ext === '.js') {
 			// js path
-			data = require(file);
+			data = require(abs);
 
 			if (typeof data === 'function') {
 				data = await data();
 			}
 		} else {
-			const fileContents = await fs.readFile(file, 'utf8');
+			const fileContents = await fs.readFile(abs, 'utf8');
 
 			switch (ext) {
 				// json path
 				case '.json':
-					data = parseJson(fileContents, file);
+					data = parseJson(fileContents, abs);
 					break;
 				// yaml paths
 				case '.yaml':
@@ -57,7 +57,7 @@ module.exports = async function quaff(rawPath) {
 
 		if (data) {
 			// remove the leading path, split into a list, and filter out empty strings
-			const dirs = path.relative(cwd, dir).split(path.sep).filter(Boolean);
+			const dirs = dir.split(path.sep).filter(Boolean);
 
 			// add the filename to the path part list
 			dirs.push(name);
