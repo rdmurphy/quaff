aa60b875cf8dfd41f18c79106513ac3e976bc6ce
Ryan Murphy <rdmurphy@users.noreply.github.com>
2020-07-13 02:52:58 -0700

    Add checks to prevent duplicate keys

--- index.js
+++ index.js
@@ -11,15 +11,53 @@ const totalist = require('totalist');
 const yaml = require('js-yaml');
 
 module.exports = async function quaff(rawPath) {
+	const validExtensions = [
+		'.js',
+		'.json',
+		'.yaml',
+		'.yml',
+		'.csv',
+		'.tsv',
+		'.aml',
+	];
+
+	// normalize the input path
 	const cwd = path.normalize(rawPath);
 
-	const payload = {};
+	// the object we will eventually return with data
+	const output = {};
+
+	// a set to watch out for duplicate keys
+	const existing = new Set();
 
 	await totalist(cwd, async (rel, abs) => {
 		const { name, dir, ext } = path.parse(rel);
 
+		// early exit if not a valid extension
+		if (!validExtensions.includes(ext)) return;
+
+		// remove the leading path, split into a list, and filter out empty strings
+		const dirs = dir.split(path.sep).filter(Boolean);
+
+		// add the filename to the path part list
+		dirs.push(name);
+
+		// build a unique "key" for this file so we can watch out for dupes
+		const key = dirs.join('.');
+
+		// if this key isn't unique, throw an error
+		if (existing.has(key)) {
+			throw new Error(
+				`More than one file has attempted to use "${key}" as its path. This is caused by having multiple files in a directory with the same name but different extensions.`,
+			);
+		}
+
+		// otherwise save it for checking future inputs
+		existing.add(key);
+
 		let data;
 
+		// we give JavaScript entries a special treatment
 		if (ext === '.js') {
 			// js path
 			data = require(abs);
@@ -28,6 +66,7 @@ module.exports = async function quaff(rawPath) {
 				data = await data();
 			}
 		} else {
+			// otherwise look for matches
 			const fileContents = await fs.readFile(abs, 'utf8');
 
 			switch (ext) {
@@ -55,16 +94,8 @@ module.exports = async function quaff(rawPath) {
 			}
 		}
 
-		if (data) {
-			// remove the leading path, split into a list, and filter out empty strings
-			const dirs = dir.split(path.sep).filter(Boolean);
-
-			// add the filename to the path part list
-			dirs.push(name);
-
-			dset(payload, dirs, data);
-		}
+		dset(output, dirs, data);
 	});
 
-	return payload;
+	return output;
 };
